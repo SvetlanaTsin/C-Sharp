9c2bd682bf80121ea74d8c5dea90e0a49d8c6f77
Svetlana <tsinovskaya@gmail.com>
2023-10-07 18:26:14 +0400

    Добавила случ числа

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 2;
-int numberB = 10;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);
+System.Console.WriteLine(numberA); 
+System.Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
