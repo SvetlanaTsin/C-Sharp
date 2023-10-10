b9fc254a4560cb0da0d63bcd3bd5bb1a8c714c1e
Svetlana <tsinovskaya@gmail.com>
2023-10-10 13:03:39 +0400

    Задача решена, новая попытка решения в отдельном файле

---
+++ Ex/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int xa = 1, ya = 20;   
+Console.SetCursorPosition(xa, ya);
+System.Console.WriteLine("+");
+
+int xb = 60, yb = 20;
+Console.SetCursorPosition(xb, yb);
+System.Console.WriteLine("+");
+
+int xc = 30, yc = 1;
+Console.SetCursorPosition(xc, yc);
+System.Console.WriteLine("+");
+
+int x = xa + xb / 2;
+int y = ya + yb / 8;
+
+Console.SetCursorPosition(x, y);
+System.Console.WriteLine("+");
+
